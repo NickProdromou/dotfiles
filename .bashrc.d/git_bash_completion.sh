a5f8ff9071b14a1fe7df09577720500c4c31a397
NickProdromou <nick.prodromou1@gmail.com>
2019-08-19 12:05:27 +1000

    remove log

--- .bashrc.d/git_bash_completion.sh
+++ .bashrc.d/git_bash_completion.sh
@@ -1,7 +1,4 @@
 # Sets up git branch and command completion (refers to separate file)
 if [ -f ~/$BASH_DIR/config/git-bash-completion.sh ]; then
-
-  echo "TEST"
-
 	source ~/$BASH_DIR/config/git-bash-completion.sh
 fi
