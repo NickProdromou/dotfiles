f70d251b0e82e7b60186eb54114eda792ffdac89
NickProdromou <nick.prodromou1@gmail.com>
2019-08-19 12:42:29 +1000

    add itermocil

---
+++ .bashrc.d/.bashrc.d
@@ -0,0 +1,1 @@
+/Users/nickpee/dotfiles/.bashrc.d
--- init_link.bash
+++ init_link.bash
@@ -1,6 +1,6 @@
 # create a symlink to root, so I can version control :D
 
 ln -s ~/dotfiles/.bashrc.d ~/.bashrc.d
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.itermocil ~/.itermocil
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile 
+ln -s ~/dotfiles/.itermocil-files ~/.itermocil
 
