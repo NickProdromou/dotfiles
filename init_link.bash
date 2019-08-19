ee098724ca14b5430cfbf92a9dc6c02a442c415b
NickProdromou <nick.prodromou1@gmail.com>
2019-08-19 11:58:29 +1000

    fix symlinking

--- init_link.bash
+++ init_link.bash
@@ -1,2 +1,4 @@
-ln -s ./.bashrc.d ~/.bashrc.d
-ln -s ./.bash_profile ~/.bash_profile
+# create a symlink to root, so I can version control :D
+
+ln -s ~/dotfiles/.bashrc.d ~/.bashrc.d
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
